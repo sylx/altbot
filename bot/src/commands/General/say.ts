54e302baedc35762bd7bc45121d6d8e98a596374
sylx <sharpoon@gmail.com>
2023-12-03 09:46:15 +0900

    use dicord.js 14.12.x

--- bot/src/commands/General/say.ts
+++ bot/src/commands/General/say.ts
@@ -1,5 +1,5 @@
 import { Category } from "@discordx/utilities"
-import { ApplicationCommandOptionType, CommandInteraction, Message } from "discord.js"
+import { ApplicationCommandOptionType, CommandInteraction, GuildMember, Message } from "discord.js"
 import { Client, SlashChoice, SlashOption } from "discordx"
 import { resolveDependency, simpleErrorEmbed, simpleSuccessEmbed } from "@utils/functions"
 import { Discord, Slash } from "@decorators"
@@ -10,16 +10,36 @@ import { Tts } from "../../services/Tts"
 export default class SayCommand {
 
 	@Slash({ 
-		description: "喋ります",
+		description: "喋ります ver2",
 		name: 'say'
 	})
 	async say(
 		@SlashOption({ name: 'text', type: ApplicationCommandOptionType.String, required: true,description: "なんか言わせたいこと" }) text: string,
 		@SlashOption({ name: 'count', type: ApplicationCommandOptionType.Integer, required: false,description: "何回言わせるか(1-10)" }) count: number,
+		@SlashOption({ name: '名前呼びチェック', type: ApplicationCommandOptionType.Mentionable,description: "対象者の名前を呼ぶ",required: false }) target_member: GuildMember,
 		interaction: CommandInteraction,
 		client: Client,
 		{ localize }: InteractionData
 	) {
+		if(target_member){
+			await simpleSuccessEmbed(
+				interaction,
+				JSON.stringify({
+					GuildMember: {
+						id: target_member.id,
+						nickname: target_member.nickname,
+						displayName: target_member.displayName,
+					},
+					User: {
+						id: target_member.user.id,
+						username: target_member.user.username,
+						tag: target_member.user.tag,
+						globalname: target_member.user.globalName
+					}
+				},null,2)
+			)
+			return
+		}
 		const tts=await resolveDependency(Tts)
 		if(!count || count < 1) count=1
 		if(count>10) count=10
