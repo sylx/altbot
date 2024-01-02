b83d8a3b9b6c5e5f16a84eb894d74430e6a33ff8
sylx <sharpoon@gmail.com>
2024-01-02 14:38:32 +0900

    backward compatiibity(for test)

--- bot/grpc/transcription_pb.d.ts
+++ bot/grpc/transcription_pb.d.ts
@@ -440,3 +440,53 @@ export namespace KeywordSpottingResponse {
     }
 
 }
+
+export class DiscordOpusPacket extends jspb.Message { 
+    getTimestamp(): number;
+    setTimestamp(value: number): DiscordOpusPacket;
+    getData(): Uint8Array | string;
+    getData_asU8(): Uint8Array;
+    getData_asB64(): string;
+    setData(value: Uint8Array | string): DiscordOpusPacket;
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): DiscordOpusPacket.AsObject;
+    static toObject(includeInstance: boolean, msg: DiscordOpusPacket): DiscordOpusPacket.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: DiscordOpusPacket, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): DiscordOpusPacket;
+    static deserializeBinaryFromReader(message: DiscordOpusPacket, reader: jspb.BinaryReader): DiscordOpusPacket;
+}
+
+export namespace DiscordOpusPacket {
+    export type AsObject = {
+        timestamp: number,
+        data: Uint8Array | string,
+    }
+}
+
+export class DiscordOpusPacketList extends jspb.Message { 
+    clearPacketsList(): void;
+    getPacketsList(): Array<DiscordOpusPacket>;
+    setPacketsList(value: Array<DiscordOpusPacket>): DiscordOpusPacketList;
+    addPackets(value?: DiscordOpusPacket, index?: number): DiscordOpusPacket;
+    getIsFinal(): boolean;
+    setIsFinal(value: boolean): DiscordOpusPacketList;
+
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): DiscordOpusPacketList.AsObject;
+    static toObject(includeInstance: boolean, msg: DiscordOpusPacketList): DiscordOpusPacketList.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: DiscordOpusPacketList, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): DiscordOpusPacketList;
+    static deserializeBinaryFromReader(message: DiscordOpusPacketList, reader: jspb.BinaryReader): DiscordOpusPacketList;
+}
+
+export namespace DiscordOpusPacketList {
+    export type AsObject = {
+        packetsList: Array<DiscordOpusPacket.AsObject>,
+        isFinal: boolean,
+    }
+}
